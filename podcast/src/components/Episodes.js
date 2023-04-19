8cd9ed7d09484f19a0910aeb7d7588b9ac4ebe9b
juliaedyck <juliaedyck@gmail.com>
2023-04-19 08:08:58 +0200

    jamilah

--- podcast/src/components/Episodes.js
+++ podcast/src/components/Episodes.js
@@ -2,56 +2,68 @@ import CloseButton from "react-bootstrap/CloseButton";
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 import James from "../photos/james_2.jpg";
-import Square1 from "../photos/squares/square_1_small.jpg"
-import Square2 from "../photos/squares/square_2_small.jpg"
-import Square3 from "../photos/squares/square_3_small.jpg"
-
+import Square1 from "../photos/squares/square_1_small.jpg";
+import Square2 from "../photos/squares/square_2_small.jpg";
+import Square3 from "../photos/squares/square_3_small.jpg";
 
 const Episodes = () => {
   return (
     <Background>
-        <Close>
-
-      <a href="/">
-        <CloseButton aria-label="Hide" />
-      </a>
+      <Close>
+        <a href="/">
+          <CloseButton aria-label="Hide" />
+        </a>
       </Close>
       <Wrapper>
-   <Squares>
-   <Img src={Square1} alt=" " />
-   <Img src={Square2} alt=" " />
-   <Img src={Square3} alt=" " />
-
-
-
-   </Squares>
+        <Squares>
+          <Img src={Square1} alt=" " />
+          <Img src={Square2} alt=" " />
+          <Img src={Square3} alt=" " />
+        </Squares>
         <Scroll>
           <EpisodeContainer>
             <Heading> EPISODE 1:</Heading>
-  
+
             <StyledNavLink to="/SkinTone"> SKIN TONE</StyledNavLink>
 
-
-            <Des> skin tone AKA James Goddard discusses jazz music, improvisation, and their potential social implications. </Des>
+            <Des>
+              {" "}
+              skin tone AKA James Goddard discusses jazz music, improvisation,
+              and their potential social implications.{" "}
+            </Des>
           </EpisodeContainer>
 
           <EpisodeContainer>
-          <Heading> EPISODE 2: </Heading>
+            <Heading> EPISODE 2: </Heading>
             <StyledNavLink to="/Sarah">SARAH FELDMAN</StyledNavLink>
-   
 
-          <Des>Sarah discusses her practice as a musician including production process, and her relationship with pop music. </Des>
-
-
+            <Des>
+              Sarah discusses her practice as a musician including production
+              process, and her relationship with pop music.{" "}
+            </Des>
           </EpisodeContainer>
 
           <EpisodeContainer>
-          <Heading> EPISODE 3: </Heading>
+            <Heading> EPISODE 3: </Heading>
             <StyledNavLink to="/Jann"> JANN TOMARO</StyledNavLink>
 
-          <Des>Jann speaks to the relationship between sound and the nervous system and her practice of listening.
- </Des>
+            <Des>
+              Jann speaks to the relationship between sound and the nervous
+              system and her practice of listening.
+            </Des>
+          </EpisodeContainer>
 
+          <EpisodeContainer>
+            <Heading>EPISODE 4: </Heading>
+            <StyledNavLink to="/Jamilah">
+              {" "}
+              JAMILAH MALIKA ABU-BAKARE
+            </StyledNavLink>
+            <Des>
+              We spoke with Jamilah about sound as haptic and embodied material
+              and how care could be demonstrated through sound, and listening to
+              gain agency through systems of power. {" "}
+            </Des>
           </EpisodeContainer>
 
           {/* <EpisodeContainer>
@@ -67,13 +79,7 @@ const Episodes = () => {
             <StyledNavLink to="/Backxwash">LISTEN</StyledNavLink>
           </EpisodeContainer>
 
-          <EpisodeContainer>
-          <Heading> EPISODE : JAMILAH MALIKA ABU-BAKARE </Heading>
-          <Des>a short descripttion </Des>
-
-            <StyledNavLink to="/Jamilah">LISTEN</StyledNavLink>
-          </EpisodeContainer>
-
+   
    
 
           <EpisodeContainer>
@@ -106,7 +112,6 @@ const Episodes = () => {
             <StyledNavLink to="/EpisodePage">LISTEN</StyledNavLink>
           </EpisodeContainer> */}
         </Scroll>
-
       </Wrapper>
     </Background>
   );
@@ -126,18 +131,31 @@ const Scroll = styled.div`
   flex-direction: column;
   justify-content: flex-start;
   align-items: flex-start;
-  /* overflow: scroll; */
+  margin-top: 38%;
+
+  @media (min-width: 760px) {
+    max-width: 700px;
+    position: relative;
+    margin-left: 50%;
+    right: 0;
+    margin-top: 2%;
+  }
+  
+  @media(max-width:290px) {
+  margin-top: 210px;
+    
+  }
 `;
 const StyledNavLink = styled(NavLink)`
-font-family:  'DM Serif Display', serif;
-font-style: italic;
-color: var(--color-green);
-/* margin-top: 2%; */
-margin-bottom: 2%;
+  font-family: "DM Serif Display", serif;
+  font-style: italic;
+  color: var(--color-green);
+  /* margin-top: 2%; */
+  margin-bottom: 2%;
 
-font-size: 20px;
+  font-size: 20px;
 
-&:hover {
+  &:hover {
     color: var(--color-orange);
     /* transition: 300ms ease-in-out; */
     cursor: pointer;
@@ -148,7 +166,6 @@ const Img = styled.img`
   max-width: 30%;
 
   @media (min-width: 768px) {
-
     max-height: 255px;
     max-width: 255px;
   }
@@ -162,20 +179,20 @@ const Wrapper = styled.div`
   height: 100%;
 
   @media (min-width: 768px) {
-flex-direction: row;
-justify-content: space-evenly;
-align-items: space-evenly;
-max-height: 100vh;
-
+    flex-direction: row;
+    /* justify-content: space-between; */
+    /* align-items: space-between; */
+    max-height: 100vh;
+    max-width: 100%;
   }
-  `;
+`;
 
 const Background = styled.div`
-height: 100vh;
-background-color: var(--color-blue);
-overflow:scroll
-
-`
+  height: 100vh;
+  max-width: 100%;
+  background-color: var(--color-blue);
+  overflow: scroll;
+`;
 const Close = styled.div`
   display: flex;
   justify-content: flex-end;
@@ -187,34 +204,42 @@ const Close = styled.div`
 `;
 
 const Heading = styled.h1`
-font-family:  'DM Serif Display', serif;
-font-style: italic;
-color: var(--color-green);
-margin-bottom: 2%;
-font-size: 20px;
-`
-
+  font-family: "DM Serif Display", serif;
+  font-style: italic;
+  color: var(--color-green);
+  margin-bottom: 2%;
+  font-size: 20px;
+`;
 
 const Des = styled.h2`
-font-family: var(--font-body);
-color: var(--color-green);
-font-size: 18px;
-`
+  font-family: var(--font-body);
+  color: var(--color-green);
+  font-size: 18px;
+`;
 
 const Squares = styled.div`
-width: 100%;
-max-height: 30%;
-display: flex;
-flex-direction: row;
-justify-content: space-evenly;
-margin-bottom: 1%;
+  width: 100%;
+  max-height: 30%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  margin-bottom: 1%;
+  position: fixed;
+top: 0;
+margin-top: 2%;
 
-@media (min-width: 768px) {
-flex-direction: column;
-max-height: 100%;
-max-width:50%;
+  @media (min-width: 768px) {
+    flex-direction: column;
+    max-height: 100%;
+    max-width: 50%;
+    /* position: sticky; */
+    position: fixed; /* Fixed Sidebar (stay in place on scroll) */
+  z-index: 1; /* Stay on top */
+  top: 0; /* Stay at the top */
+  left: 0;
+margin-top: 0px;
+
   }
-`
-
+`;
 
 export default Episodes;
--- podcast/src/components/Jamilah.js
+++ podcast/src/components/Jamilah.js
@@ -1,10 +1,25 @@
 import CloseButton from "react-bootstrap/CloseButton";
 import styled from "styled-components";
-import Photo from "../photos/jamilah.jpg";
+import Photo from "../photos/jamilah_square.jpg";
+import { Link } from "react-router-dom";
+import PDF from "../components/transcripts/A Kind of Harmony Ep.4_Jamilah Malika Abu-Bakare_transcript.pdf"
+import { BsInstagram } from "react-icons/bs";
+import { RiPatreonLine } from "react-icons/ri";
 
 const Jamilah = () => {
+
+  const newWindow = (url) => {
+    window.open(url, "newwindow");
+    return false;
+  };
+
+  const embedUrl ="https://open.spotify.com/embed/episode/1SAQ3djsWCXAFLksTzRnjo?utm_source=generator"
+
+
   return (
     <>
+      <Background>
+
       <a href="Episodes">
         <Close>
           {" "}
@@ -13,10 +28,40 @@ const Jamilah = () => {
       </a>
 
       <Wrapper>
-        EPISODE : JAMILAH MALIKA ABU-BAKARE
+     <Heading>EPISODE 4: JAMILAH MALIKA ABU-BAKARE</Heading>   
         <Img src={Photo} alt="Jamilah malika abu-bakare " />
-        audio
-        <p>
+        <IFrameMobile
+            src={embedUrl}
+            className="iframe"
+            title="a Kind of Harmony"
+            frameBorder="0"
+            wmode="transparent"
+            ratio="8:1"
+            style={{
+              position: "relative",
+              border: "0",
+              width: "100%",
+              height: "150px",
+            }}
+            data-name="pb-iframe-player"
+          />
+
+          <IFrameDesk
+            src={embedUrl}
+            className="iframe"
+            title="a Kind of Harmony"
+            frameBorder="0"
+            wmode="transparent"
+            ratio="8:1"
+            style={{
+              position: "relative",
+              border: "0",
+              width: "30%",
+              height: "150px",
+            }}
+            data-name="pb-iframe-player"
+          />
+        <Des>
           jamilah malika abu-bakare is an artist and writer contemplating
           refusal, repetition, dedication, and intimacy through sound art, video
           essay, text off-page and/or installation. whatever the form, she
@@ -26,11 +71,39 @@ const Jamilah = () => {
           Canada, including Contemporary Field Gallery (Vancouver,) Circuit
           Gallery (Toronto), and Artscape (Peterborough). her writing most
           recently appeared in Canadian Art magazine. her first curatorial
-          project AURAL ALTERITIES is on view at auralalterirites.com.
-        </p>
-  
-        <p>links</p>
+          project AURAL ALTERITIES is on view at 
+          
+          <StyledLink
+              onClick={() =>
+                newWindow("https://www.auralalterities.com/")
+              }
+              target="_blank"
+            >
+            www.auralalterities.com.
+            </StyledLink>
+            </Des>
+            <Transcript href = {PDF} target= "blank"> TRANSCRIPTION</Transcript> 
+<Des>
+            <StyledLink
+              onClick={() =>
+                newWindow("https://www.instagram.com/akindofharmony/")
+              }
+              target="_blank"
+            >
+              <BsInstagram />
+            </StyledLink>
+            <StyledLink
+              onClick={() =>
+                newWindow("https://www.patreon.com/aKindofHarmony")
+              }
+              target="_blank"
+            >
+              <RiPatreonLine />
+            </StyledLink>
+            </Des>
       </Wrapper>
+      </Background>
+
     </>
   );
 };
@@ -62,4 +135,65 @@ const Close = styled.div`
   justify-content: flex-end;
   padding: 1%;
 `;
+
+const Heading = styled.h1`
+  font-family: "DM Serif Display", serif;
+  font-style: italic;
+  color: var(--color-green);
+  margin-bottom: 2%;
+  font-size: 20px;
+`;
+const StyledLink = styled.a`
+  margin-left: 3px;
+  margin-right: 3px;
+
+  color: var(--color-green);
+  &:hover {
+    color: var(--color-orange);
+    /* transition: 300ms ease-in-out; */
+    cursor: pointer;
+  }
+`;
+const Des = styled.h2`
+  font-family: var(--font-body);
+  margin-bottom: 2%;
+  padding-left: 5%;
+  padding-right: 5%;
+  color: var(--color-green);
+  font-size: 18px;
+`;
+
+const Background = styled.div`
+  height: 100vh;
+  background-color: var(--color-blue);
+  overflow: scroll;
+`;
+
+
+const IFrameMobile = styled.iframe`
+  @media (min-width: 768px) {
+display: none;
+  }
+`
+
+const IFrameDesk = styled.iframe`
+padding-bottom: none;
+margin-bottom: none;
+  @media (max-width: 768px) {
+display: none;
+  }
+`
+
+const Transcript = styled.a`
+font-family:  'DM Serif Display', serif;
+font-style: italic;
+color: var(--color-green);
+margin-bottom: 2%;
+margin-top: 2%;
+font-size: 20px;
+
+&:hover {
+    color: var(--color-pink);
+  }
+`
 export default Jamilah;
